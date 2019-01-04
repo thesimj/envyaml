0ae28c031774e49a2f2e47d54089eb693bcaa76b
Mykola Bubelich <thesimj@users.noreply.github.com>
2019-01-05 00:11:53 +0100

    update setup and travic ci file

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
