79a220cabb69d71397310d941de205610cf5e4cb
Mykola Bubelich <thesimj@users.noreply.github.com>
2020-06-26 15:58:12 +0200

    fixing Travis/CI deployment

--- setup.py
+++ setup.py
@@ -29,9 +29,9 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
     ],
 )
